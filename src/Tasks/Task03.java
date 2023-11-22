67b9d4d92fef7ca989086e68f2a2b6183ae5f81c
NounceIWNL <nounceiwnl@gmail.com>
2023-11-22 10:39:14 +0300

    Task 03 was finished

--- src/Tasks/Task03.java
+++ src/Tasks/Task03.java
@@ -2,6 +2,8 @@ package Tasks;
 
 public class Task03 {
     public static void main(String[] args) {
-        
+        double numberToRound = 2.7;
+        long a = Math.round(numberToRound);
+        System.out.println(a);
     }
 }
