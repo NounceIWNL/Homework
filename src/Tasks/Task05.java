44ecb34239c38b297964fe3694380d29492fb6f5
NounceIWNL <nounceiwnl@gmail.com>
2023-11-22 11:09:44 +0300

    Task 05 was finished

---
+++ src/Tasks/Task05.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+public class Task05 {
+    public static void main(String[] args) {
+        final double RADIUS = 10;
+        double square = Math.PI * RADIUS * RADIUS;
+        double circleLength = Math.PI * RADIUS * 2;
+        System.out.println("Площадь круга: " + square);
+        System.out.println("Длина окружности: " + circleLength);
+    }
+}
