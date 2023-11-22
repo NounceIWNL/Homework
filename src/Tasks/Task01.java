2950fd265783ee659fc87919b1f789d5912192ce
NounceIWNL <nounceiwnl@gmail.com>
2023-11-22 09:55:30 +0300

    123

---
+++ src/Tasks/Task01.java
@@ -0,0 +1,10 @@
+package Tasks;
+
+public class Task01 {
+    public static void main(String[] args) {
+        double x = 5, y = 3;
+        System.out.println(x / y);
+        System.out.println(x % y);
+        System.out.println(Math.sqrt(x));
+    }
+}
---
+++ src/Tasks/Task02.java
@@ -0,0 +1,7 @@
+package Tasks;
+
+public class Task02 {
+    public static void main(String[] args) {
+
+    }
+}
