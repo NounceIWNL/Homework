84dfd9a3eff30a1b07184a4268aea5cdee742e4b
NounceIWNL <nounceiwnl@gmail.com>
2023-11-22 11:10:12 +0300

    Task 06 was finished

---
+++ src/Tasks/Task06.java
@@ -0,0 +1,12 @@
+package Tasks;
+
+public class Task06 {
+    public static void main(String[] args) {
+        final double WIDTH = 10;
+        final double HEIGHT = 15;
+        double perimeter = (WIDTH * 2) + (HEIGHT * 2);
+        double square = WIDTH * HEIGHT;
+        System.out.println("Площадь прямоугольника: " + square);
+        System.out.println("Периметр прямоугольнгика: " + perimeter);
+    }
+}
