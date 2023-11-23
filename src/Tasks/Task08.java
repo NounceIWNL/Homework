7142b4e0bc86c77acdcf07f1bd196b2e21e6c50d
NounceIWNL <nounceiwnl@gmail.com>
2023-11-23 13:28:41 +0300

    Task 08 was finished

---
+++ src/Tasks/Task08.java
@@ -0,0 +1,10 @@
+package Tasks;
+
+public class Task08 {
+    public static void main(String[] args) {
+        final double TIME_BETWEEN_LIGHT_AND_THUNDER = 6;
+        final double SPEED_OF_SOUND = 0.34; // using km/s
+        double distanceOfLightningStrike = TIME_BETWEEN_LIGHT_AND_THUNDER * SPEED_OF_SOUND;
+        System.out.println(distanceOfLightningStrike);
+    }
+}
