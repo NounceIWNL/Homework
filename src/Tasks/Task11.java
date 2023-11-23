1cf02cdbb6bd9ae120ae3704f9f430ff160e5322
NounceIWNL <nounceiwnl@gmail.com>
2023-11-23 15:11:21 +0300

    Task 11 was finished

---
+++ src/Tasks/Task11.java
@@ -0,0 +1,15 @@
+package Tasks;
+
+public class Task11 {
+    public static void main(String[] args) {
+        int givenTime = 1757845; // in seconds
+        int theWholeDays = givenTime / 3600 / 24;
+        int remainingSecondsOfDays = givenTime - theWholeDays * 3600 * 24;
+        int theWholeHours = remainingSecondsOfDays / 3600;
+        int remainingSecondsOfHours = remainingSecondsOfDays - theWholeHours * 3600;
+        int theWholeMinutes = remainingSecondsOfHours / 60;
+        int remainingSecondsOfMinutes = remainingSecondsOfHours - theWholeMinutes * 60;
+        System.out.println(theWholeDays + " days, " + theWholeHours + " hours, " + theWholeMinutes + " minutes, " + remainingSecondsOfMinutes + " seconds.");
+    }
+} 
+
