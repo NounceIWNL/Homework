00d2663a13b68cebad339704ba7d5908908fff5f
NounceIWNL <nounceiwnl@gmail.com>
2023-11-23 13:48:42 +0300

    Task 09 was finished

---
+++ src/Tasks/Task09.java
@@ -0,0 +1,14 @@
+package Tasks;
+import java.util.Scanner;
+
+public class Task09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the number: ");
+        double number = scanner.nextDouble();
+        System.out.println(number);
+        int numberAsInt = (int) number;
+        boolean result = number == numberAsInt;
+        System.out.println(result);
+    }
+}
