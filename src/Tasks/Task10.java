01a99f24a2f6205788fa22aa04b2d6d9b13044d3
NounceIWNL <nounceiwnl@gmail.com>
2023-11-23 14:22:19 +0300

    Task 10 was finished

---
+++ src/Tasks/Task10.java
@@ -0,0 +1,22 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String[] args) {
+        final int WEIGHT = 100;
+        final int HEIGHT = 193;
+        int perfectWeight = HEIGHT - 110;
+        if (WEIGHT != perfectWeight) {
+            int weightDifference = perfectWeight - WEIGHT;
+            if (weightDifference < 0) {
+                System.out.println("You need to lose " + weightDifference + " kg.");
+            } else {
+                System.out.println("You need to gain " + (perfectWeight - WEIGHT) + " more kg.");
+            }
+        } else {
+            System.out.println("Your current weight is perfect!");
+        }
+    }
+}
+
