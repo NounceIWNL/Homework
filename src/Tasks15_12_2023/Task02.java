062ea36fb8bc6c33a54ce4ed10db70c9c652c06f
NounceIWNL <nounceiwnl@gmail.com>
2023-11-29 14:15:44 +0300

    Task02 was finished

---
+++ src/Tasks15_12_2023/Task02.java
@@ -0,0 +1,25 @@
+package Tasks15_12_2023;
+
+import java.util.Scanner;
+
+public class Task02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input any number from 0 to 24: ");
+        int hourNumber = scanner.nextInt();
+        System.out.println();
+
+        if (hourNumber >= 0 && hourNumber < 6) {
+            System.out.println("Good night");
+        } else if (hourNumber >= 6 && hourNumber < 12) {
+            System.out.println("Good morning");
+        } else if (hourNumber >= 12 && hourNumber < 18) {
+            System.out.println("Good afternoon");
+        } else if (hourNumber >= 18 && hourNumber <= 24) {
+            System.out.println("Good evening");
+        } else {
+            System.out.println("Wrong number. Please try again.");
+        }
+
+    }
+}
