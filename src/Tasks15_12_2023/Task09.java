20b2f26b64d4751cb60ace4ee2e40e7c1c0a4f71
NounceIWNL <nounceiwnl@gmail.com>
2023-12-02 17:46:48 +0300

    Task09 was finished

---
+++ src/Tasks15_12_2023/Task09.java
@@ -0,0 +1,12 @@
+package Tasks15_12_2023;
+
+public class Task09 {
+    public static void main(String[] args) {
+        double floors = 9;
+        double apartmentsAmountOnOneFloor = 4;
+        double apartmentNumber = 96;
+        double apartmentsAmountOnOneEntrance = floors * apartmentsAmountOnOneFloor;
+        double entranceNumber = Math.ceil(apartmentNumber / apartmentsAmountOnOneEntrance);
+        System.out.println(Math.round(entranceNumber));
+    }
+}
