2b89374cc3389846fb2ce154c60083ff77d762a0
NounceIWNL <nounceiwnl@gmail.com>
2023-12-07 17:07:25 +0300

    Task 11 was finished

---
+++ src/Tasks15_12_2023/Task11.java
@@ -0,0 +1,40 @@
+package Tasks15_12_2023;
+
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        Scanner scannerA = new Scanner(System.in);
+        System.out.println("Input a: ");
+        double a = scannerA.nextDouble();
+        System.out.println();
+        Scanner scannerB = new Scanner(System.in);
+        System.out.println("Input b: ");
+        double b = scannerB.nextDouble();
+        System.out.println();
+        Scanner scannerC = new Scanner(System.in);
+        System.out.println("Input c: ");
+        double c = scannerC.nextDouble();
+        System.out.println();
+
+        double d = (b * b) - 4 * a * c; // d - дискриминант
+
+        double x1 = (- b + Math.sqrt(d)) / (2 * a);
+        double x2 = (- b - Math.sqrt(d)) / (2 * a);
+
+        if (d > 0)
+            System.out.println("The following square roots are: " + x1 + " and " + x2);
+        else if (d == 0)
+            System.out.println("The following square roots are: " + x1 + " and " + x2);
+        else
+            System.out.println("No square roots.");
+
+
+
+
+
+
+
+
+    }
+}
