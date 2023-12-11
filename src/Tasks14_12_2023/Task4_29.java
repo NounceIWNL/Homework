62219a5ed146c2427eae9dd638183384f85e6975
NounceIWNL <nounceiwnl@gmail.com>
2023-12-11 17:24:00 +0300

    Task 4_29 was created

---
+++ src/Tasks14_12_2023/Task4_29.java
@@ -0,0 +1,15 @@
+package Tasks14_12_2023;
+
+import java.util.Scanner;
+
+public class Task4_29 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input three-digit number of your ticket: ");
+        double inputNumber = scanner.nextDouble();
+
+        double squareOfScanner = Math.sqrt(inputNumber);
+        
+
+    }
+}
