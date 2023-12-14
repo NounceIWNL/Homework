ae7702f712d59d02d11caf72a09d496065596d2e
NounceIWNL <nounceiwnl@gmail.com>
2023-12-14 15:32:50 +0300

    Task 4_3 was finished

---
+++ src/Tasks14_12_2023/Task4_3.java
@@ -0,0 +1,12 @@
+package Tasks14_12_2023;
+
+public class Task4_3 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+
+        if (x <= 4)
+            System.out.println("This is area 1.");
+        else System.out.println("This is area 2.");
+    }
+}
