00aebe5d2d450d24c2fcafcacc32efe06120ff05
NounceIWNL <nounceiwnl@gmail.com>
2023-12-14 15:39:01 +0300

    Task 4_10 was finished

---
+++ src/Tasks14_12_2023/Task4_10.java
@@ -0,0 +1,12 @@
+package Tasks14_12_2023;
+
+public class Task4_10 {
+    public static void main(String[] args) {
+        double circleRadius = 10;
+        double squareSide = 10;
+
+        if (squareSide >= circleRadius * 2)
+            System.out.println("Square area is greater than circle area.");
+        else System.out.println("Circle are is greater than square area.");
+    }
+}
