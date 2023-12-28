a084d62bdac8c853a943dd10e74b1a1766ab612f
NounceIWNL <nounceiwnl@gmail.com>
2023-12-28 17:38:07 +0300

    Task 5_86 has been finished

--- src/Tasks28_12_2023/Task5_86.java
+++ src/Tasks28_12_2023/Task5_86.java
@@ -1,7 +1,18 @@
 package Tasks28_12_2023;
 
+import java.util.Scanner;
+
 public class Task5_86 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите натуральное число n: ");
+        int n = scanner.nextInt();
 
+        int summary = 0;
+        for (int i = 1; i <= (2 * n - 1); i = i + 2) {
+            summary += i;
+            System.out.println(i);
+        }
+        System.out.println("Квадрат числа " + n + " равен " + summary);
     }
 }
