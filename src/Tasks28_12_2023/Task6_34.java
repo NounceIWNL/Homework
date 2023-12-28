47139c7bb66648cb2cb3ab41f04f8c790e86ce18
NounceIWNL <nounceiwnl@gmail.com>
2023-12-28 17:44:57 +0300

    Task 6_34 has been finished

--- src/Tasks28_12_2023/Task6_34.java
+++ src/Tasks28_12_2023/Task6_34.java
@@ -2,6 +2,15 @@ package Tasks28_12_2023;
 
 public class Task6_34 {
     public static void main(String[] args) {
+        int countOfNumbers = 0;
+        int currentNumber = 100;
 
+        while (countOfNumbers < 15) {
+            if (currentNumber % 19 == 0) {
+                System.out.println(currentNumber);
+                countOfNumbers++;
+            }
+            currentNumber++;
+        }
     }
 }
