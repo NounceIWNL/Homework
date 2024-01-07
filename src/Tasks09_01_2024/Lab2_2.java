74b237050f26356664215c85c07027ba300049d6
NounceIWNL <nounceiwnl@gmail.com>
2024-01-07 14:38:50 +0300

    Task 2_2 has been finished

--- src/Tasks09_01_2024/Lab2_2.java
+++ src/Tasks09_01_2024/Lab2_2.java
@@ -1,30 +1,40 @@
 package Tasks09_01_2024;
 
+//вычисление факториала через метод.
+// Значение result равно 6 , т.к. по условию необходимо сразу высчитать факториал 4! и т.д.
+
+
 public class Lab2_2 {
+    static int calculateFactorial(int n) {
+        int result = 6;
+        for (int i = 4; i < n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
+
         double x = 0.5; //заданный Х
 
-        double a = x; //последующее значение после начального
+        double a = 0.625; //последующее значение после начального
 
-        double sum = x; //финальная сумма всего ряда
+        double sum = a; //финальная сумма всего ряда
 
         double eps = 1e-5; //граница расчетов
 
-        int i = 2; //степень
-
-        int factorial = 2;
+        int i = 3; //степень
 
         while (Math.abs(a) > eps) {
-            for (int j = 1; j < ; j++) {
-
-
-                a *= -x * (3 * i - 4) / (3 * i);
-                sum += a;
-                i *= 2 - 1;
-                System.out.println();
-            }
+            a *= -x * (2 * i + x) / calculateFactorial(2 * i);
+            sum += a;
+            i = i * 2 - 1;
+            System.out.println(calculateFactorial(2 * i));
+            System.out.println(sum);
         }
         System.out.println(sum);
         System.out.println(Math.sin(x) - Math.cos(x) + 1);
     }
 }
+
+
