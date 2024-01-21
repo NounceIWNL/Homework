8478e3a8e84de248790a32a8d4886c0036a759d2
NounceIWNL <nounceiwnl@gmail.com>
2024-01-21 16:19:07 +0300

    Task has been finished and uploaded.

--- src/Tasks22_01_2024/HumanBeing.java
+++ src/Tasks22_01_2024/HumanBeing.java
@@ -2,25 +2,34 @@ package Tasks22_01_2024;
 
 // Мною будет использован класс "Человек"
 
+// p.s. Возможно, я неправильно понял трактовку условия,
+// поэтому здесь использованы сеттеры, геттеры и toString() одновременно
+
 public class HumanBeing {
     public static void main(String[] args) {
 
-        Person person1 = new Person();
+        Person person1 = new Person("Mike", 56);
+        System.out.println(person1);
+
         person1.setNameAndAge("Mike", 56);
-//        person1.introduction();
         System.out.println("Hello! My name is " + person1.getName() + ", I'm " + person1.getAge() + " years old.");
 
-        Person person2 = new Person();
+
+        Person person2 = new Person("Walter", 51);
+        System.out.println(person2);
+
         person2.setNameAndAge("Walter", 51);
-//        person2.introduction();
         System.out.println("Hello! My name is " + person2.getName() + ", I'm " + person2.getAge() + " years old.");
-
-
     }
 
     static class Person {
         private String name;
         private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
 
         public void setNameAndAge(String name, int age) {
             if (name.isEmpty()) {
@@ -43,22 +52,9 @@ public class HumanBeing {
             return age;
         }
 
-//        public void introduction() {
-//            System.out.println("Hello! My name is " + name + ", I'm " + age + " years old.");
-//        }
-
-        class Human{
-            String name;
-            int age;
-
-            private void setName(String myName){
-                name = myName;
-            }
-            private void setAge(int myAge){
-                age = myAge;
-            }
+        public String toString() {
+            return "Hello! My name is " + name + ", I'm " + age + " years old.";
         }
-
     }
 }
 
