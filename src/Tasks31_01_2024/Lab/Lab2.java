8352d8ce2c1e1c0cede4b3d07f74286b8051a1ea
NounceIWNL <nounceiwnl@gmail.com>
2024-01-28 18:58:57 +0300

    Updated

--- src/Tasks31_01_2024/Lab/Lab2.java
+++ src/Tasks31_01_2024/Lab/Lab2.java
@@ -2,25 +2,25 @@ package Tasks31_01_2024.Lab;
 
 public class Lab2 {
     public static void main(String[] args) {
-        int size = 3;
+        int size = 5;
         int rows = size * 2; // строки
         int cols = size * 2 - 1; // столбцы
 
         int[][] arr = new int[rows][cols];
 
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size - 1; j++) {
-                arr[i][j] = j + 3 - i;
+            for (int j = 0; j <= i; j++) {
+                arr[i][j] = 5 + j - i;
                 System.out.print(arr[i][j] + " ");
 
             }
         }
-//        for (int i = size; i < rows; i++) {
-//            for (int j = i - size; j < size; j++) {
-//                arr[i][j] = size - j + Math.abs(size - i);
-//                System.out.print(arr[i][j] + " ");
-//            }
-//        }
+        for (int i = rows - 3; i = size; i--) {
+            for (int j = 0; j < Math.abs(size - i); j++) {
+                arr[i][j] = 5 - j;
+                System.out.print(arr[i][j] + " ");
+            }
+        }
         System.out.println();
         System.out.println();
         for (int i = 0; i < rows; i++) {
