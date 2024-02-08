939d3e9ab69a26045fff489db16e03351149cf5c
NounceIWNL <nounceiwnl@gmail.com>
2024-02-08 17:08:28 +0300

    updated

--- src/Tasks12_02_2024/Task01.java
+++ src/Tasks12_02_2024/Task01.java
@@ -2,14 +2,13 @@ package Tasks12_02_2024;
 
 public class Task01 {
     public static void main(String[] args) {
-        array(4);
+        System.out.println(array(4));
 
     }
 
     public static int array(int number) {
-        if (number == 0)
-            System.out.println(number);
-        array(number - 1);
-        return number;
+        if (number == 1)
+            return 1;
+        return number * array(number - 1);
     }
 }
