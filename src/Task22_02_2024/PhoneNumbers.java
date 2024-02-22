81446f63f4ea7676721696dff98f72fcb4bb7f5a
NounceIWNL <nounceiwnl@gmail.com>
2024-02-22 16:35:23 +0300

    Updated

--- src/Task22_02_2024/PhoneNumbers.java
+++ src/Task22_02_2024/PhoneNumbers.java
@@ -10,25 +10,27 @@ public class PhoneNumbers {
         phoneBook.put("Walter", "89123456789");
         phoneBook.put("Mike", "89234567890");
         phoneBook.put("Jane", "89345678901");
+        phoneBook.put("Joe", "56785439012");
+        phoneBook.put("Keil", "42130900212");
 
-        // Определенная цифра, которой должен начинаться номер
+
         char digit = '8';
 
-        // Создание списка для хранения номеров, начинающихся с определенной цифры
+
         List<String> phonesWithDigit = new ArrayList<>();
 
-        // Поиск и добавление номеров в список
+
         for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
             if (entry.getValue().charAt(0) == digit) {
                 phonesWithDigit.add(entry.getValue());
             }
         }
 
-        // Сортировка номеров по фамилии
+
         Collections.sort(phonesWithDigit);
 
-        // Вывод отсортированных номеров
-        System.out.println("Номера телефонов, начинающиеся с цифры " + digit + ":");
+
+        System.out.println("Phone numbers starting with digit " + digit + ":");
         for (String phone : phonesWithDigit) {
             System.out.println(phone);
         }
