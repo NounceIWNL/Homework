79d78b08a0dbbaa7816af9bd543d44e902a69448
NounceIWNL <nounceiwnl@gmail.com>
2024-02-28 06:27:56 +0300

    initial commit

---
+++ src/Tasks12_02_2024/Task01_01.java
@@ -0,0 +1,26 @@
+package Tasks12_02_2024;
+
+import java.util.Arrays;
+
+public class Task01_01 {
+    public static void main(String[] args) {
+
+        int numbers = 5;
+
+        int[] array = new int[numbers];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+        recursion(array);
+
+
+    }
+
+     public static void recursion(int[] numbers){
+
+
+
+    }
+}
