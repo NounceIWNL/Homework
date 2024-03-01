050113814da4e2a76f0789643718ed00e8ba5fdc
NounceIWNL <nounceiwnl@gmail.com>
2024-03-01 04:32:24 +0300

    initial commit

--- src/Tasks12_02_2024/Task01_01.java
+++ src/Tasks12_02_2024/Task01_01.java
@@ -2,25 +2,48 @@ package Tasks12_02_2024;
 
 import java.util.Arrays;
 
+// 0 1 2 3 4
+
 public class Task01_01 {
     public static void main(String[] args) {
 
         int numbers = 5;
-
         int[] array = new int[numbers];
 
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
         }
-
-        recursion(array);
-
+        recursion(array, null);
 
     }
 
-     public static void recursion(int[] numbers){
+
+    public static void recursion(int[] numbers, Integer currentIndex) {
+
+        int[] arrayCopy = Arrays.copyOf(numbers, numbers.length);
+        if (currentIndex != null) {
+            int currentElementToSwap;
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println("i = " + i);
+                System.out.println("currentIndex=" + currentIndex);
+                System.out.println('b');
+                if (currentIndex == i)
+                    continue;
 
 
 
+                System.out.println(Arrays.toString(numbers));
+
+                numbers = Arrays.copyOf(arrayCopy, arrayCopy.length);
+            }
+        } else {
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println(i);
+                System.out.println('a');
+                currentIndex = i;
+                recursion(arrayCopy, currentIndex);
+            }
+
+        }
     }
 }
