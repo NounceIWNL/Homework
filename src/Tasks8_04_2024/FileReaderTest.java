74bb007af80d8da8a3501041707144712099e567
NounceIWNL <nounceiwnl@gmail.com>
2024-04-04 03:44:32 +0300

    Updated

--- src/Tasks8_04_2024/FileReaderTest.java
+++ src/Tasks8_04_2024/FileReaderTest.java
@@ -5,16 +5,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class FileReaderTest {
+public class FileReaderTest extends DomesticChemical {
+    public FileReaderTest(String name, double price) {
+        super(name, price);
+    }
+
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\Users\\a\\Downloads\\JavaMarathon2021\\Homework\\src\\Tasks8_04_2024\\ProductList.txt");
 
 //        System.out.println(readProductList(file));
 
         List<String> productList = new ArrayList<>(readProductList(file));
-//        System.out.println(getProductListFirst(productList));
-//        System.out.println(getProductListSecond(productList));
-        System.out.println(getProductListThird(productList));
+
+        getProductList(productList);
     }
 
     public static List<String> readProductList(File file) throws IOException {
@@ -23,45 +26,44 @@ public class FileReaderTest {
         List<String> stringList = new ArrayList<>();
         while ((st = br.readLine()) != null) {
             stringList.add(st);
-        } return stringList;
+        }
+        return stringList;
     }
 
-    public static String getProductListFirst(List<String> productList) throws IOException {
+    public static String getProductList(List<String> productList) throws IOException {
         String getProduct = null;
-        for (int i = 0; i < productList.size(); i++) {
-            if(productList.get(i).startsWith("Одежда:")){
-                break;
-            }
-            getProduct = productList.get(i);
-            System.out.println(getProduct);
-        } return getProduct;
-    }
+        String[] itemInArray = new String[]{};
+        String[] priceInArray = new String[]{};
+        String itemPrice = null;
+        String itemName = null;
 
-    public static String getProductListSecond(List<String> productList) throws IOException {
-        String getProduct = null;
         for (int i = 0; i < productList.size(); i++) {
-            if(productList.get(i).startsWith("Алкогольные напитки:")){
-                break;
-            }
-            getProduct = productList.get(i);
-            if(productList.get(i).startsWith("Бытовая химия:")){
+            if (productList.get(i).isEmpty()) {
                 continue;
             }
-            System.out.println(getProduct);
-        } return getProduct;
-    }
 
-    public static String getProductListThird(List<String> productList) throws IOException {
-        String getProduct = null;
-        for (int i = 0; i < productList.size(); i++) {
-            if(productList.get(i).startsWith("Бытовая химия:")){
-                continue;
-            }
-            if(productList.get(i).startsWith("Одежда:")){
-                continue;
-            }
             getProduct = productList.get(i);
-            System.out.println(getProduct);
-        } return getProduct;
+            itemInArray = getProduct.split(":");
+            priceInArray = itemInArray[1].split("-");
+            itemPrice = priceInArray[1];
+            itemName = priceInArray[0];
+
+            if (productList.get(i).startsWith("Бытовая химия:")) {
+                System.out.println(getProduct);
+                DomesticChemical domesticChemical1 = new DomesticChemical(itemName.trim(),Double.parseDouble(itemPrice.trim()));
+                System.out.println(domesticChemical1.name + "\n" + domesticChemical1.price);
+            }
+            if (productList.get(i).startsWith("Одежда:")) {
+                System.out.println(getProduct);
+                Clothing clothing1 = new Clothing(itemName.trim(),Double.parseDouble(itemPrice.trim()));
+                System.out.println(clothing1.name + "\n" + clothing1.price);
+            }
+            if (productList.get(i).startsWith("Алкогольные напитки:")) {
+                System.out.println(getProduct);
+                Beverage beverage1 = new Beverage(itemName.trim(),Double.parseDouble(itemPrice.trim()));
+                System.out.println(beverage1.name + "\n" + beverage1.price);
+            }
+        }
+        return getProduct;
     }
 }
