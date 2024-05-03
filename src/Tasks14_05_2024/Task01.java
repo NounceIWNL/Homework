df1ec263c08456a718b1ca17310dfab0b761f49c
NounceIWNL <nounceiwnl@gmail.com>
2024-05-03 15:33:33 +0300

    typo fixed

--- src/Tasks14_05_2024/Task01.java
+++ src/Tasks14_05_2024/Task01.java
@@ -1,5 +1,15 @@
 package Tasks14_05_2024;
 
+/*
+Два охранника попеременно стоят на посту по 12 часов.
+Пока один стоит на посту, другой спит.
+У обоих охранников имеются наручные часы. Охранник на посту сверяется со временем,
+будит в нужный момент спящего и сам засыпает.
+Процесс повторяется.
+Должны быть классы Guard, Watch
+ */
+
+
 public class Task01 {
     public static void main(String[] args) {
         Guard guard1 = new Guard();
@@ -7,12 +17,17 @@ public class Task01 {
 
         Thread thread1 = new Thread(guard1);
         Thread thread2 = new Thread(guard2);
+
+        thread1.start();
     }
 
     static class Guard implements Runnable {
 
         @Override
         public void run() {
+            for (int i = 0; i < 12; i++) {
+                System.out.println("First guard is active for " + i + " hour.");
+            }
 
         }
     }
