a139ec4c94bfed53a6af4906257c9dd6e49eaa14
NounceIWNL <nounceiwnl@gmail.com>
2024-07-09 21:31:19 +0300

    html homework has been added

--- src/Tasks14_05_2024/Task01.java
+++ src/Tasks14_05_2024/Task01.java
@@ -11,39 +11,71 @@ package Tasks14_05_2024;
 
 
 public class Task01 {
-    public static void main(String[] args) {
-        Guard guard1 = new Guard();
-        Guard guard2 = new Guard();
+    static volatile boolean firstGuardIsOnDuty;
+    static volatile boolean secondGuardIsOnDuty;
+    public static void main(String[] args) throws InterruptedException {
+        Watch watch = new Watch();
+        Thread thread3 = new Thread(watch);
 
+
+        Guard guard1 = new Guard("Guard 1");
         Thread thread1 = new Thread(guard1);
-        Thread thread2 = new Thread(guard2);
-
         thread1.start();
+//      thread1.join();
 
+        thread3.start();
+        thread3.join();
+
+        Guard guard2 = new Guard("Guard 2");
+        Thread thread2 = new Thread(guard2);
         thread2.start();
+
 
     }
 
     static class Guard implements Runnable {
+        public String guardName;
+
+        public Guard(String guardName) {
+            this.guardName = guardName;
+        }
 
         @Override
         public void run() {
-            for (int i = 0; i < 12; i++) {
-                System.out.println("First guard is active for " + i + " hour.");
+            guardIsActive(true);
+        }
+
+        public void guardIsActive(boolean check) {
+            if (check) {
+                System.out.println(this.guardName + " is active for ");
+            } else
+                guardIsAsleep(true);
+        }
+
+        public void guardIsAsleep(boolean check) {
+            if (check) {
+                System.out.println(this.guardName + " is asleep for ");
+            }
+        }
+    }
+
+    static class Watch implements Runnable {
+
+        @Override
+        public void run() {
+            int i = 1;
+            do {
+                System.out.println(i + " hour");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-            }
+                i++;
+                if (i == 13) {
+                    i = 1;
+                }
+            } while (true);
         }
-
-        public void firstGuard() {
-            System.out.println("First guard is active for ");
-        }
-    }
-
-    static class Watch {
-        private volatile boolean timer;
     }
 }
